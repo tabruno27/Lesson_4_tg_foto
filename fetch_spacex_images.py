5cb79b0daeeb1d25078bee335a3c598832e91cbc
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-03-05 16:46:53 +0200

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -9,15 +9,24 @@ def fetch_spacex_images(launch_id=None):
     url = f"https://api.spacexdata.com/v5/launches/{launch_id}" if launch_id else "https://api.spacexdata.com/v5/launches/latest"
     response = requests.get(url)
 
-    if response.status_code == 200:
-        launch_data = response.json()
-        image = launch_data.get('links', {}).get('flickr', {}).get('original')
+   if not response.ok:
+        print(f"Ошибка при получении данных: {response.status_code}")
+        return
+       
+    launch_data = response.json()
+    image = launch_data.get('links', {}).get('flickr', {}).get('original')
 
+     if not images:
+        print("Изображения не найдены для данного запуска.")
+        return
+        
         image_folder = utils.image_folder
-        create_image_folder(image_folder)
-
-        for index, img_url in enumerate(image, start=1):
-            download_image(img_url, image_folder, f"spacex{index + 1}.jpg")
+        
+        for index, img_url in enumerate(images, start=1):
+        try:
+            download_image(img_url, image_folder, f"spacex_{index}.jpg")
+        except Exception as e:
+            print(f"Ошибка при скачивании изображения {img_url}: {e}")
 
 
 if __name__ == "__main__":
