3114e4a982efcf0f8d0b5781fe0f2c1601a77707
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-03-07 09:33:12 +0200

    Update fetch_apod_images.py

--- fetch_apod_images.py
+++ fetch_apod_images.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from urllib.parse import urlsplit, unquote
 from os.path import splitext
 import utils
-from utils import create_image_folder, download_image
+from utils import download_image
 
 
 def get_file_extension(url):
