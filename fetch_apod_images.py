1c5ab9227a777e8c52abe60fff97c82b284ebab0
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-03-13 12:54:01 +0200

    Update fetch_apod_images.py

--- fetch_apod_images.py
+++ fetch_apod_images.py
@@ -3,8 +3,7 @@ import os
 from dotenv import load_dotenv
 from urllib.parse import urlsplit, unquote
 from os.path import splitext
-import utils
-from utils import download_image
+from utils import download_image, get_image_folder
 
 
 def get_file_extension(url):
@@ -14,42 +13,39 @@ def get_file_extension(url):
     return extension
 
 
-def fetch_apod_images(api_key, count=None):
-
+def fetch_apod_images(nasa_api_key, count=30):
     url = f"https://api.nasa.gov/planetary/apod"
     response = requests.get(url, params={
-        "api_key": api_key,
-        'count": count
+        "api_key": nasa_api_key,
+        "count": count
     })
 
     if not response.ok:
         print(f"Ошибка: {response.status_code}")
         return
 
-    image_data = response.json()
-    image_folder = utils.image_folder
+    apod_image_data = response.json()
+    image_folder = get_image_folder()
 
-    for index, image in enumerate(image_data):
+    for index, image in enumerate(apod_image_data, start=1):
         img_url = image.get('url')
         if not img_url:
             print("Изображение не найдено в ответе APOD.")
-            continue 
+            continue
 
         file_extension = get_file_extension(img_url)
 
-         try:
-            download_image(img_url, image_folder, f"apod_image_{index + 1}{file_extension}")
+        try:
+            download_image(img_url, image_folder, f"apod_image_{index}{file_extension}")
         except (requests.HTTPError, requests.ConnectionError) as e:
             print(f"Ошибка при скачивании изображения {img_url}: {e}")
-        except FileNotFoundError as e:
-            print(f"Ошибка: не удалось найти файл для сохранения изображения: {e}")
-            
+
 
 def main():
     load_dotenv()
-    api_key = os.getenv('NASA_API_KEY')
+    nasa_api_key = os.getenv('NASA_API_KEY')
 
-    fetch_apod_images(count=30, api_key)
+    fetch_apod_images(nasa_api_key, count=30)
 
 if __name__ == "__main__":
     main()
