eefb356259b324de37e072c3e99b7b788c37e148
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-03-13 12:53:48 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,12 +10,10 @@ def download_image(url, save_folder, filename):
         file.write(img_response.content)
     return file_path
 
-if __name__ == "__main__":
-    image_folder="images"
-    os.makedirs(folder_name, exist_ok=True)
+def get_image_folder():
+    return "images"
 
-    try:
-        file_path = download_image(image_url, image_folder, image_filename)
-        print(f"Изображение успешно скачано: {file_path}")
-    except requests.exceptions.RequestException as e:
-        print(f"Ошибка при скачивании изображения: {e}")
+if __name__ == "__main__":
+    image_folder = get_image_folder()
+    os.makedirs(image_folder, exist_ok=True)
+
